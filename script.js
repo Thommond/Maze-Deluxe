a654ea88d9929d514a2f84c85eb953391fc63c2d
Thom Mondeaux <Thommond554@gmail.com>
2019-12-06 20:25:20 -0500

    I attempted to add the animation for the moving object. I also added some css to the table

--- script.js
+++ script.js
@@ -38,45 +38,71 @@ const clearElement = (element) => {
 
 const drawMaze = (maze) => {
   //creating a function
+  let mover = document.createElement('div');
+  let divTable = document.getElementById('cover');
+  console.log(divTable);
+  divTable.appendChild(mover);
+  mover.style.backgroundColor = 'black';
+  mover.style.width = '15px';
+  mover.style.height = '15px';
+  mover.style.position = 'absolute';
+  mover.setAttribute('id', 'player');
+
+
+
+
   let tableEl = document.querySelector('table');
   //setting table El to the element table
   clearElement(tableEl);
   //clearing if any elements are children of the tableEl
 
-//loop which
+  //loop which
   for (let i = 0; i < maze.length; i++) {
 
     let rowEl = document.createElement('tr');
 
     tableEl.appendChild(rowEl);
+    rowEl.style.border = 'none';
 
-    rowEl.style.border = '1px solid black'
-//loop which
+    rowEl.style.border = '1px solid black';
+    //loop which
     for (let x = 0; x < maze[i].length; x++) {
 
       let tdEl = document.createElement('td');
 
       tdEl.innerHTML = maze[i].charAt(x);
-//conditionals below if/else if the char is a specific character
-//then run the code below
+      //conditionals below if/else if the char is a specific character
+      //then run the code below
+
+
       if (maze[i].charAt(x) == "#") {
         rowEl.appendChild(tdEl);
-         tdEl.style.backgroundColor = 'red';
-
+        tdEl.style.backgroundColor = 'red';
+        tdEl.style.color = 'red';
+        tdEl.style.border = 'none';
       } else if (maze[i].charAt(x) == ".") {
         rowEl.appendChild(tdEl);
         tdEl.style.backgroundColor = "blue";
-
+        tdEl.style.color = 'blue';
+        tdEl.style.border = 'none';
       } else if (maze[i].charAt(x) == "_") {
         rowEl.appendChild(tdEl);
         tdEl.style.backgroundColor = "green";
-
+        tdEl.style.color = 'green';
+        tdEl.style.border = 'none';
       } else if (maze[i].charAt(x) == "!") {
         rowEl.appendChild(tdEl);
         tdEl.style.backgroundColor = 'yellow';
+        tdEl.style.color = 'yellow';
+        tdEl.style.border = 'none';
+
+
+
       }
 
+
     }
+
 
   };
 
@@ -87,56 +113,77 @@ drawMaze(maze1);
 
 
 
-/*const cells = document.getElementsByTagName('td');
-console.log(cells)
-console.log(cells[0]);
-console.log(cells[1]);*/
 
-
-
-//The code for the mover is below
-/*
-//selecting the p element with the Id of mover
-let mover = document.getElementById("mover");
-//an eventListener to add ability to move
-
+let mover = document.getElementById('player');
 
 window.addEventListener("keydown", event => {
-  //location is equal to zero
-  let location = 0;
-//condition which states that if key "w" is pressed then anything with in will occur
-if (event.key == "w") {
-//setting the binding newLocation to the location binding minus 20
-console.log(location)
-  let newLocation = location - 20;
-  console.log(newLocation)
-  //mover.style.top is being set equal to the newLocation binding plus the string of px;
-  mover.style.top = newLocation + "px";
 
-console.log("test");
+    //condition which states that if key is pressed then anything with in will occur
+    if (event.key == "w") {
+
+      let animate = (time, lastTime) => {
+        if (lastTime != null) {
+          (time - lastTime) * 0.01;
+        }
+        mover.style.left = 5 + 'px';
+        requestAnimationFrame(newTime => animate(newTime, time));
+      }
+      requestAnimationFrame(animate);
+    }
+
+
+
+   else if (event.key == "s") {
+
+    let animate = (time, lastTime) => {
+      if (lastTime != null) {
+        (time - lastTime) * 0.01;
+      }
+      mover.style.left = 5 + 'px';
+      requestAnimationFrame(newTime => animate(newTime, time));
+    }
+    requestAnimationFrame(animate);
+    }
+
+  }
+
+
+
+
 }
-
-else if (event.key == "s") {
-
-  let newLocation = location + 20;
-  mover.style.top = newLocation + "px";
-
-console.log("test2");
-}
-
 else if (event.key == "a") {
-  let newLocation = location -  20;
-  mover.style.left = newLocation + "px";
-console.log("test3");
-}
 
-else if (event.key == "d") {
-  let newLocation = location + 20;
-  mover.style.left = newLocation + "px";
-  console.log(mover.style.left)
-console.log("test4");
-} });
-
+  let animate = (time, lastTime) => {
+    if (lastTime != null) {
+      (time - lastTime) * 0.01;
+    }
+    mover.style.left = 5 + 'px';
+    requestAnimationFrame(newTime => animate(newTime, time));
+  }
+  requestAnimationFrame(animate);
+  }
 
 }
-*/
+
+
+
+
+} else if (event.key == "d") {
+
+  let animate = (time, lastTime) => {
+    //want object to move on press
+      //while
+    if (lastTime != null) {
+      (time - lastTime) * 0.01;
+    }
+    mover.style.left = 5 + 'px';
+    requestAnimationFrame(newTime => animate(newTime, time));
+  }
+  requestAnimationFrame(animate);
+  }
+
+}
+
+}
+
+});
